1ae17cb869a81a6001f023d21ec59fed58f75f0f
a.ahamiane <a.ahamiane@xefi.fr>
2024-05-28 14:56:17 +0200

    Make the sidebar fixed

--- src/layout/DailyAppsLayout.tsx
+++ src/layout/DailyAppsLayout.tsx
@@ -1,55 +1,56 @@
-import { DailyAppsNavbar } from '@/navbar/Navbar';
-import { cn } from '@/utils';
-import { useState } from 'react';
-import { DailyAppsLayoutProps } from '@/types';
-import { Sidebar } from '@/sidebar/Sidebar';
+import { DailyAppsNavbar } from "@/navbar/Navbar";
+import { cn } from "@/utils";
+import { useState } from "react";
+import { DailyAppsLayoutProps } from "@/types";
+import { Sidebar } from "@/sidebar/Sidebar";
 
 export const DailyAppsLayout = ({
-	header,
-	children,
-	classNameContainer,
-	sidebar,
+  header,
+  children,
+  classNameContainer,
+  sidebar,
 }: DailyAppsLayoutProps) => {
-	const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-	const setOpenDropdown = (isOpen: boolean) => {
-		setIsOpen(isOpen);
-	};
+  const setOpenDropdown = (isOpen: boolean) => {
+    setIsOpen(isOpen);
+  };
 
-	return (
-		// eslint-disable-next-line tailwindcss/no-custom-classname
-		<div className='bg-foreground min-h-screen w-full overflow-x-auto'>
-			<DailyAppsNavbar
-				applicationName={header.applicationName}
-				accountNavigation={header.accountNavigation}
-				user={header.user}
-				className={header.className}
-				toggleTheme={header.toggleTheme}
-				setOpenDropdown={setOpenDropdown}
-				isOpenDropdown={isOpen}
-				Logo={header.Logo}
-			/>
-			<div className='relative z-20 flex w-full'>
-				<Sidebar
-					btnIcon={sidebar.btnIcon}
-					navigation={sidebar.navigation}
-					pathname={sidebar.pathname}
-					className='flex mt-16 fixed inset-y-0 md:mt-0 md:sticky left-0 z-10 w-14 flex-col border-r min-h-screen border-border background-sidebar'
-					isOpenDropdown={isOpen}
-				/>
+  return (
+    // eslint-disable-next-line tailwindcss/no-custom-classname
+    <div className="bg-foreground min-h-screen w-full overflow-x-auto">
+      <DailyAppsNavbar
+        applicationName={header.applicationName}
+        accountNavigation={header.accountNavigation}
+        user={header.user}
+        className={header.className}
+        toggleTheme={header.toggleTheme}
+        setOpenDropdown={setOpenDropdown}
+        isOpenDropdown={isOpen}
+        Logo={header.Logo}
+      />
+      <div className="relative z-20 flex w-full">
+        <Sidebar
+          btnIcon={sidebar.btnIcon}
+          navigation={sidebar.navigation}
+          pathname={sidebar.pathname}
+          className="flex mt-16 fixed inset-y-0 md:mt-0 md:sticky left-0 z-10 w-14 flex-col border-r min-h-screen border-border background-sidebar"
+          isOpenDropdown={isOpen}
+        />
 
-				<main
-					// fixer le responsive endessous se 300px
-					className={cn(
-						'ml-24 mr-10 md:mr-16 md:ml-16 mt-28 flex-1 bg-background min-h-[83vh] h-full relative ',
-						classNameContainer
-					)}>
-					{children}
-				</main>
-				{isOpen && (
-					<div className='absolute top-0 w-screen min-h-screen backdrop-blur-sm -z-10' />
-				)}
-			</div>
-		</div>
-	);
+        <main
+          // fixer le responsive endessous se 300px
+          className={cn(
+            "ml-24 mr-10 md:mr-16 md:ml-16 mt-28 flex-1 bg-background min-h-[83vh] h-full relative ",
+            classNameContainer
+          )}
+        >
+          {children}
+        </main>
+        {isOpen && (
+          <div className="absolute top-0 w-screen min-h-screen backdrop-blur-sm -z-10" />
+        )}
+      </div>
+    </div>
+  );
 };
--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -1,54 +1,57 @@
-import { SidebarProps } from '@/types';
-import { cn } from '@/utils';
-import { CustomButton } from '@/button/CustomButton';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { SidebarMenu } from './SidebarMenu';
+import { SidebarProps } from "@/types";
+import { cn } from "@/utils";
+import { CustomButton } from "@/button/CustomButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { SidebarMenu } from "./SidebarMenu";
 
 export const Sidebar = ({
-	navigation,
-	pathname,
-	className,
-	btnIcon,
-	isOpenDropdown,
+  navigation,
+  pathname,
+  className,
+  btnIcon,
+  isOpenDropdown,
 }: SidebarProps) => {
-	return (
-		<aside
-			className={cn('md:min-w-[290px] md:w-[290] md:px-2', className, {
-				'min-w-[290px] w-[290px] px-2': isOpenDropdown,
-			})}>
-			<div className='md:sticky top-16'>
-				<nav className='flex flex-col items-center gap-4 px-3 mt-4'>
-					<CustomButton
-						icon={
-							<FontAwesomeIcon
-								icon={btnIcon as IconProp}
-								className={cn('h-5 w-5 md:h-6 md:w-6', {
-									'h-6 w-6': isOpenDropdown,
-								})}
-							/>
-						}
-						iconPosition='left'
-						className={cn('bg-primary px-2 h-9 md:w-full', {
-							'w-full': isOpenDropdown,
-						})}>
-						<span
-							className={cn('hidden md:block font-semibold', {
-								block: isOpenDropdown,
-							})}>
-							Ajouter un évènement
-						</span>
-					</CustomButton>
-					<div className='bg-sidebar w-full h-[1px]' />
+  return (
+    <aside
+      className={cn("md:min-w-[290px] md:w-[290px] md:px-2", className, {
+        "min-w-[290px] w-[290px] px-2": isOpenDropdown,
+      })}
+    >
+      <div className="sticky top-16 md:fixed md:w-[270px]  ">
+        <nav className="flex flex-col items-center gap-4 px-3 mt-4">
+          <CustomButton
+            icon={
+              <FontAwesomeIcon
+                icon={btnIcon as IconProp}
+                className={cn("h-5 w-5 md:h-6 md:w-6", {
+                  "h-6 w-6": isOpenDropdown,
+                })}
+              />
+            }
+            iconPosition="left"
+            className={cn("bg-primary px-2 h-9 md:w-full", {
+              "w-full": isOpenDropdown,
+            })}
+          >
+            <span
+              className={cn("hidden md:block font-semibold", {
+                block: isOpenDropdown,
+              })}
+            >
+              Ajouter un évènement
+            </span>
+          </CustomButton>
+          <div className="bg-sidebar w-full h-[1px]" />
 
-					<SidebarMenu
-						navigation={navigation}
-						pathname={pathname}
-						className='w-full'
-						isOpenDropdown={isOpenDropdown}
-					/>
-				</nav>
-			</div>
-		</aside>
-	);
+          <SidebarMenu
+            navigation={navigation}
+            pathname={pathname}
+            className="w-full"
+            isOpenDropdown={isOpenDropdown}
+          />
+        </nav>
+      </div>
+    </aside>
+  );
 };
